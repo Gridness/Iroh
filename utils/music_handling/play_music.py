b36405792158475a4aad9afa364ae312e73ad403
Gridness <zonddas@yahoo.com>
2021-07-03 15:36:03 +0300

    is_playing state is now working properly

--- utils/music_handling/play_music.py
+++ utils/music_handling/play_music.py
@@ -1,10 +1,10 @@
 from utils.music_handling.play_next import play_next
 import discord
-from commands.play import Music
+from utils.is_playing_state_singleton import IsPlaying
 
 async def play_music(client, vc, song_queue, FFMPEG_OPTIONS):
     if len(song_queue) > 0:
-        Music.is_playing = True
+        IsPlaying().is_paying = True
         
         extracted_url = song_queue[0][0]['source'] 
         if vc == "" or not not vc.is_connected():
@@ -15,4 +15,4 @@ async def play_music(client, vc, song_queue, FFMPEG_OPTIONS):
         song_queue.pop(0)
         vc.play(discord.FFmpegAudio(extracted_url, **FFMPEG_OPTIONS), after=lambda e: play_next(vc, song_queue, FFMPEG_OPTIONS))
     else:
-        Music.is_playing = False
+        IsPlaying().is_paying = False
--- utils/music_handling/play_next.py
+++ utils/music_handling/play_next.py
@@ -1,11 +1,11 @@
+from utils.is_playing_state_singleton import IsPlaying
 import discord
-from commands.play import Music
 
 def play_next(vc, song_queue, FFMPEG_OPTIONS):
     if len(song_queue) > 0:
-        Music.is_playing = True
+        IsPlaying().is_paying = True
         extracted_url = song_queue[0][0]['source']
         song_queue.pop(0)
         vc.play(discord.FFmpegAudio(extracted_url, **FFMPEG_OPTIONS), after=lambda e: play_next())
     else:
-        Music.is_playing = False
+        IsPlaying().is_paying= False
