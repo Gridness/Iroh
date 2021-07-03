951dcc3f5cdbb2a2fa8b9c77b82009383ada9e97
Gridness <zonddas@yahoo.com>
2021-07-03 15:58:11 +0300

    Unused files are now removed

--- utils/is_playing_state_singleton.py
+++
@@ -1,8 +0,0 @@
-class IsPlaying():
-    def __init__(self):
-        self.is_paying = False
-
-    def __new__(cls):
-        if not hasattr(cls, 'instance'):
-            cls.instance = super(IsPlaying, cls).__new__(cls)
-        return cls.instance
